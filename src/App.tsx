0e102a95345f7be62e2ed2f4b62d8e364f5c00c0
abimaelst <abimaelst@gmail.com>
2022-08-15 02:23:14 -0300

    create banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components'
+import { Banner } from './components/Banner'
 import { NavBar } from './components/NavBar'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -7,6 +8,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <NavBar />
+      <Banner />
       <div style={{ height: '2000px' }}>a</div>
       <GlobalStyle />
     </ThemeProvider>
---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+import { Container, Section } from './Banner.styles'
+
+export function Banner() {
+  return (
+    <Section>
+      <Container>
+        <h1>Abimael</h1>
+      </Container>
+    </Section>
+  )
+}
